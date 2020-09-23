5af3c679ccecfad25dbadc5b27c73a03f08f071a
Kaan Secen <24114@ma-web.nl>
2020-09-23 12:23:42 +0200

    Opdracht

---
+++ PYTB1L2InVariablesWeTrust.py
@@ -0,0 +1,28 @@
+character1 = '$'
+
+float1 = float(10)
+
+bool1 = True
+bool2 = bool1 + True
+
+int1 = int(7.6)
+int2 = int(2.9)
+int3 = int1 - int2
+
+string1 = "Hello "
+string2 = "You"
+string3 = string1 + string2
+
+print(character1)
+print(float1,"\n")
+
+print(bool1)
+print(bool2,"\n")
+
+print(int1)
+print(int2)
+print(int3,"\n")
+
+print(string1)
+print(string2)
+print(string3)
